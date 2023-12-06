b31a1fc256e0e1cb33873fd2387daf4d418caf68
Sunghyun Ko <sunko@cx2r1s1.42seoul.kr>
2023-12-06 17:25:59 +0900

    test mlx_image put

--- minirt.c
+++ minirt.c
@@ -6,17 +6,36 @@
 /*   By: sunko <sunko@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 16:30:28 by sunko             #+#    #+#             */
-/*   Updated: 2023/12/06 16:55:20 by sunko            ###   ########.fr       */
+/*   Updated: 2023/12/06 17:25:44 by sunko            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 #include "libft.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 int	main(int argc, char *argv[])
 {
 	void	*mlx;
+	t_data	img;
 	void	*mlx_win;
 
 	if (argc != 2)
@@ -26,7 +45,16 @@ int	main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "miniRT");
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	printf("img.addr = %p\n", img.addr);
+	printf("bits_per_pixel = %d\n", img.bits_per_pixel);
+	printf("line_length = %d\n", img.line_length);
+	printf("endian = %d\n", img.endian);
+
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
 
